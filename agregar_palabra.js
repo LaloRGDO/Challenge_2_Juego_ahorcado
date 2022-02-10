b3af5afb95b9b130ed4cafc1315f57b0307e0f3d
LaloRGDO <95454688+LaloRGDO@users.noreply.github.com>
2022-02-09 21:59:32 -0600

    limpieza del código del archivo agregar_palabra.js

--- agregar_palabra.js
+++ agregar_palabra.js
@@ -2,32 +2,6 @@ var palabras = ["MANZANA", "ORACLE", "ALURA", "AHORCADO","PROGRAMACION","AUTOBUS
 var palabraNueva = document.querySelector("#ingresar-palabra");
 var botonAgregar = document.querySelector("#agregar-palabra");
 
-/*
-botonAgregar.addEventListener("click", function(event){
-    event.preventDefault();
-    palabras.push(palabraNueva.value.toUpperCase());
-    palabras = localStorage.getItem("palabras");
-    console.log(palabras);
-    palabraNueva.value = "";
-});
-
-localStorage.setItem("palabras", JSON.stringify(palabras));
-var palabrasNuevas = JSON.parse(localStorage.getItem("palabras"));
-console.log(palabrasNuevas);
-*/
-/*
-console.log(palabras);
-localStorage.setItem("palabras",JSON.stringify(palabras));
-palabras = localStorage.getItem("palabras");
-palabras = JSON.parse(palabras);
-palabras.push("aguila");
-localStorage.setItem("palabras", JSON.stringify(palabras));
-palabras = localStorage.getItem("palabras");
-palabras = JSON.parse(palabras);
-palabras.push("cocodrilo");
-console.log(palabras);
-*/
-
 botonAgregar.addEventListener("click", function(event){
     event.preventDefault();
     console.log(palabras);
